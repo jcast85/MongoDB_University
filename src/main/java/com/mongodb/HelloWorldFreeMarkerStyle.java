fbf5a4d6b588604135773bb8060b2b7e5f7b4083
Juri Castellani <juri.castellani@hotmail.com>
2018-01-11 00:20:38 +0100

    First examples of FreeMarker and SparkJava usages

---
+++ src/main/java/com/mongodb/HelloWorldFreeMarkerStyle.java
@@ -0,0 +1,30 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.port;
+
+public class HelloWorldFreeMarkerStyle {
+    public static void main(String[] args) {
+        port(4567);
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(
+                HelloWorldFreeMarkerStyle.class, "/");
+        try {
+            Template helloTemplate = configuration.getTemplate("/hello.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<>();
+            helloMap.put("name", "Freemarker");
+            helloTemplate.process(helloMap, writer);
+
+            System.out.println(writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/mongodb/HelloWorldSparkFreeMarkerStyle.java
@@ -0,0 +1,45 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.halt;
+
+public class HelloWorldSparkFreeMarkerStyle {
+    public static void main(String[] args) {
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldSparkFreeMarkerStyle.class, "/");
+
+        get("/", (req, res) ->
+                getHelloWorldWriter(configuration, "Freemarker")
+        );
+
+        get("/juri", (req, res) ->
+                getHelloWorldWriter(configuration, "Juri")
+        );
+
+        get("/echo/:thing", (req, res) ->
+                getHelloWorldWriter(configuration, req.params(":thing"))
+        );
+    }
+
+    private static StringWriter getHelloWorldWriter(Configuration configuration, String name) {
+        StringWriter writer = new StringWriter();
+        try {
+            Template helloTemplate = configuration.getTemplate("/hello.ftl");
+            Map<String, Object> helloMap = new HashMap<>();
+            helloMap.put("name", name);
+            helloTemplate.process(helloMap, writer);
+        } catch (Exception e) {
+            halt(500);
+            e.printStackTrace();
+        }
+        return writer;
+
+    }
+}
