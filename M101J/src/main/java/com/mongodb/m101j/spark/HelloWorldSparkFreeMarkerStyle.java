796736960cab945572df26a91cf6272dd738b142
Juri Castellani <juri.castellani@hotmail.com>
2018-01-21 19:54:59 +0100

    Getting date from mongo collection document instead of defined map has been implemented.

--- M101J/src/main/java/com/mongodb/m101j/spark/HelloWorldSparkFreeMarkerStyle.java
+++ M101J/src/main/java/com/mongodb/m101j/spark/HelloWorldSparkFreeMarkerStyle.java
@@ -1,17 +1,23 @@
 package com.mongodb.m101j.spark;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ReadPreference;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.bson.BsonString;
+import org.bson.Document;
 
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.halt;
-import static spark.Spark.port;
+import static spark.Spark.*;
 
 public class HelloWorldSparkFreeMarkerStyle {
+    private static MongoClient client;
+
     public static void main(String[] args) {
         final Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(HelloWorldSparkFreeMarkerStyle.class, "/");
@@ -36,11 +42,21 @@ public class HelloWorldSparkFreeMarkerStyle {
 
     private static Object getHelloWorldWriter(Configuration configuration, String name) {
         StringWriter writer = new StringWriter();
+
+        MongoClientOptions.Builder builder = MongoClientOptions.builder().connectionsPerHost(100);
+        client = new MongoClient(new MongoClientURI("mongodb://m101_user:m101_user@localhost:27017/m101", builder));
+        MongoDatabase db = client.getDatabase("m101").withReadPreference(ReadPreference.secondary());
+
+        MongoCollection<Document> collection = db.getCollection("hello");
+        collection.drop();
+
+        Document document = new Document();
+        document.append("name", new BsonString("MongoDB"));
+        collection.insertOne(document);
+
         try {
             Template helloTemplate = configuration.getTemplate("/hello.ftl");
-            Map<String, Object> helloMap = new HashMap<>();
-            helloMap.put("name", name);
-            helloTemplate.process(helloMap, writer);
+            helloTemplate.process(collection.find().first(), writer);
         } catch (Exception e) {
             halt(500);
             e.printStackTrace();
